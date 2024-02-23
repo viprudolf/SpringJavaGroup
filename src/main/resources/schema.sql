2dad7ddf7b0c0daf0bd94f6bacffb7eb131e17a9
mrVierPC <maxvier.1996@gmail.com>
2024-02-23 23:45:28 +0300

    fix: fixed data and schema for h2 add: add implementation h2

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,38 +1,35 @@
-CREATE TABLE IF NOT EXISTS "FormEducation"
-(
-    id          SERIAL PRIMARY KEY,
-    createdAt   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "_nameForm" VARCHAR(255)
+create table if not exists Form_education (
+    id identity primary key,
+    created_At timestamp not null,
+    name_Form varchar(50) not null
 );
 
-CREATE TABLE IF NOT EXISTS "Qualification"
-(
-    id                   SERIAL PRIMARY KEY,
-    createdAt            TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "_nameQualification" VARCHAR(255)
+create table if not exists Qualification (
+    id identity primary key,
+    created_At timestamp not null,
+    name_Qualification varchar(50) not null
 );
 
-CREATE TABLE IF NOT EXISTS "Group"
-(
-    id                 SERIAL PRIMARY KEY,
-    createdAt          TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "_idSpeciality"    VARCHAR(255),
-    "_idQualification" VARCHAR(255),
-    "_idFormEducation" VARCHAR(255),
-    "_idFaculty"       VARCHAR(255),
-    "_idName"          VARCHAR(255),
-    "_idCourse"        VARCHAR(255),
-    "_idCountStudent"  VARCHAR(255),
-    "_idCountSubgroup" VARCHAR(255),
-    FOREIGN KEY ("_idSpeciality") REFERENCES "Speciality" (id),
-    FOREIGN KEY ("_idQualification") REFERENCES "Qualification" (id),
-    FOREIGN KEY ("_idFormEducation") REFERENCES "FormEducation" (id)
+create table if not exists Speciality (
+    id identity primary key,
+    created_At timestamp not null,
+    name_Speciality varchar(50) not null,
+    profile varchar(50) not null
 );
 
-CREATE TABLE IF NOT EXISTS "Speciality"
-(
-    id                SERIAL PRIMARY KEY,
-    createdAt         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    "_nameSpeciality" VARCHAR(255),
-    "_profile"        VARCHAR(255)
+create table if not exists GGroup (
+    id identity primary key,
+    created_At timestamp not null,
+    id_Speciality varchar(50) not null,
+    id_Qualification varchar(50) not null,
+    id_FormEducation varchar(50) not null,
+    id_Faculty varchar(50) not null,
+    id_Name varchar(50) not null,
+    id_Course varchar(50) not null,
+    id_CountStudent varchar(50) not null,
+    id_CountSubgroup varchar(50) not null,
+
+    foreign key (id_Speciality) references Speciality (id),
+    foreign key (id_Qualification) references Qualification (id),
+    foreign key (id_FormEducation) references Form_education (id)
 );
